20c9555e6c8e1bae0af17c7b80adc33bb6ef01b7
Joseph Kao <josephtkao@gmail.com>
2020-04-19 21:19:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,9 @@
 class Triangle
   # write code here
-  attr_accessor :kind
+  attr_accessor :kind, :sides
   def initialize(a, b, c)
+    @sides = %w(a,b,c).sort
+    if (@sides[0] + @sides[1] < @sides[2])
   end
 
   class TriangleError < StandardError
